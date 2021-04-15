baf0faae859629ead515d7159036feead3d5a24e
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-15 03:33:24 -0300

    Entendendo laços e variaveis com a linguagem

--- sla.c
+++ sla.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    printf("Gado!\n");
+    // Um printf basico
+    printf("\nSistema em teste, so para entender os laços em C\n");
+
+    int A = 0, B = 7, C = 4, D = 3;
+    
+    for (A = 0; A < B; A++)
+    {
+        D = D + 1;
+    }
+    // Assim que mistura strings e variaveis dentro do printf
+    printf("\nResultado para for >%d< %s", D, "\n\n");
+
+    //for (A = 0; A <= C; A++)
+    //{
+    //    D = D + 2;
+    //}
+    
+    if (D >= 20)
+    {
+        printf("\n%d %s", D, " - ");
+        printf("SISTEMA OPERACIONAIS\n");
+    }
+    else
+    {
+        printf("%d %s", D, " - ");
+        printf("REDES DE COMPUTADORES\n");
+    }
+    
     return 0;
 }
 
